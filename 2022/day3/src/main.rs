6f46dcc4b418bba1c56359416eb7b6aeecf195a0
LDprg <71488985+LDprg@users.noreply.github.com>
2023-05-06 12:31:11 +0200

    day 3 part 2

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -19,6 +19,24 @@ fn share_char(a: &str, b: &str) -> Option<char> {
     ch
 }
 
+fn share_char_3(a: &str, b: &str, c: &str) -> Option<char> {
+    let seta: HashSet<char> = a.chars().collect();
+    let setb: HashSet<char> = b.chars().collect();
+
+    let mut ch: Option<char> = None;
+
+    c.chars().find(|c| {
+        if seta.contains(c) && setb.contains(c) {
+            ch = Some(*c);
+            true
+        } else {
+            false
+        }
+    });
+
+    ch
+}
+
 fn decode(ch: char) -> Option<u8> {
     let i = ch as u8;
 
@@ -32,16 +50,32 @@ fn decode(ch: char) -> Option<u8> {
 fn main() {
     let file = File::open("./res/input_real.txt").unwrap();
 
+    let mut list : Vec<String> = Vec::new();
+
     let mut sum = 0;
 
     for line in BufReader::new(file).lines() {
         let line = line.unwrap();
 
-        let first = line.get(..line.len() / 2).unwrap();
-        let second = line.get(line.len() / 2..).unwrap();
+        list.push(line);
 
-        sum += decode(share_char(first, second).unwrap()).unwrap() as u32;
+        if list.len() >= 3 {
+            
+            sum += decode(share_char_3(&list[0], &list[1], &list[2]).unwrap()).unwrap() as u32;
+            
+
+            list.clear();
+        }
     }
+
+    // for line in BufReader::new(file).lines() {
+    //     let line = line.unwrap();
+
+    //     let first = line.get(..line.len() / 2).unwrap();
+    //     let second = line.get(line.len() / 2..).unwrap();
+
+    //     sum += decode(share_char(first, second).unwrap()).unwrap() as u32;
+    // }
 
     println!("{}", sum);
 }
