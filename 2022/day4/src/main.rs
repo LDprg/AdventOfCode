cf12d606e374c12f12323012f84468bfe17abda6
LDprg <71488985+LDprg@users.noreply.github.com>
2023-06-12 19:03:32 +0200

    day 4 part 1

--- 2022/day4/src/main.rs
+++ 2022/day4/src/main.rs
@@ -1,4 +1,3 @@
-use regex::Regex;
 use std::fs::*;
 use std::io::*;
 
@@ -10,15 +9,20 @@ fn main() {
     for line in BufReader::new(file).lines() {
         let line = line.unwrap();
 
-        for item in pattern.captures_iter(&line) {
-            if (item[1] >= item[3] && item[2] <= item[4])
-            || (item[3] >= item[1] && item[4] <= item[2])
-            {
-                sum += 1;
-                println!("found: {}-{}, {}-{}", &item[1], &item[2], &item[3], &item[4]);
-            }
+        let data = line.split(&[',', '-']).map(|str| {
+            str.parse::<u16>().unwrap()
+        }).collect::<Vec<_>>();
+            
+        let range1  = &data[0]..=&data[1];
+        let range2 = &data[2]..=&data[3];
+
+        if (range1.contains(range2.start()) && range1.contains(range2.end()))
+            || (range2.contains(range1.start()) && range2.contains(range1.end()))
+        {
+            sum += 1;
+            println!("Found: {}-{}, {}-{}", range1.start(), range1.end(), range2.start(), range2.end());
         }
     }
 
-    println!("\n{}", sum); 
+    println!("\n{}", sum);
 }
