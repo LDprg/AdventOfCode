f406d812d4be8f301be2b83e267fdefe8bee9cf2
LDprg <71488985+LDprg@users.noreply.github.com>
2023-06-12 19:13:35 +0200

    day 4 part 2

--- 2022/day4/src/main.rs
+++ 2022/day4/src/main.rs
@@ -9,18 +9,25 @@ fn main() {
     for line in BufReader::new(file).lines() {
         let line = line.unwrap();
 
-        let data = line.split(&[',', '-']).map(|str| {
-            str.parse::<u16>().unwrap()
-        }).collect::<Vec<_>>();
-            
-        let range1  = &data[0]..=&data[1];
+        let data = line
+            .split(&[',', '-'])
+            .map(|str| str.parse::<u16>().unwrap())
+            .collect::<Vec<_>>();
+
+        let range1 = &data[0]..=&data[1];
         let range2 = &data[2]..=&data[3];
 
-        if (range1.contains(range2.start()) && range1.contains(range2.end()))
-            || (range2.contains(range1.start()) && range2.contains(range1.end()))
+        if (range1.end() >= range2.start() && range1.end() <= range2.end())
+            || (range2.end() >= range1.start() && range2.start() <= range1.end())
         {
             sum += 1;
-            println!("Found: {}-{}, {}-{}", range1.start(), range1.end(), range2.start(), range2.end());
+            println!(
+                "Found: {}-{}, {}-{}",
+                range1.start(),
+                range1.end(),
+                range2.start(),
+                range2.end()
+            );
         }
     }
 
